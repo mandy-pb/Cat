b1ffa0ade8746ad345bd6741b59ce983123943de
Mandy PB <plaisance.mandy@gmail.com>
2023-04-18 20:34:27 -0400

    Commented out js innerText

--- js/script.js
+++ js/script.js
@@ -4,6 +4,6 @@ var button = document.querySelector(".show-cat");
 var cat = document.querySelector(".cat");
 //console.log(cat);
 button.addEventListener("click", function () {
-  button.innerText = "It's working!";
+  //button.innerText = "It's working!";
   cat.classList.add("show");
 });
